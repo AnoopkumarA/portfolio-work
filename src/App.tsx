0997a43dea3250f6fb61140534a1d8d8bcc68b81
Pillzen Pvt. Ltd <129652248+Pillzen@users.noreply.github.com>
2024-08-27 11:13:20 +0530

    a

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css'
 
 import Home from "./pages/home";
-import { BrowserRouter as  Routes, Route, HashRouter} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from "./components/Navbar/Navbar";
 
 
@@ -11,13 +11,13 @@ const about: React.FC = () => {
     return (
         <>
            <div className="bg-[#020516] ">
-           <HashRouter>
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
          
         </Routes>
-        </HashRouter>
+      </Router>
 
       
       
