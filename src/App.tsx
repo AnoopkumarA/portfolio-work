8f9ba275b05ff69bbf26e9e4274ad87e98ed7c1e
Anoop Kumar A <92391766+AnoopkumarA@users.noreply.github.com>
2024-08-27 11:35:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css'
 
 import Home from "./pages/home";
-import { BrowserRouter as Router, Routes, Route, HashRouter} from 'react-router-dom'
+import { BrowserRouter as  Routes, Route, HashRouter} from 'react-router-dom'
 import Navbar from "./components/Navbar/Navbar";
 
 
